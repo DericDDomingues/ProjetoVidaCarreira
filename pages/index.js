88ca00c255878bba1e053290331b6b263b3db7d8
DericDDomingues <102702356+DericDDomingues@users.noreply.github.com>
2022-05-26 12:02:28 -0300

    primeiroCodigo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,33 @@
+import Head from 'next/head'
+
 function Home(){
-    return <div><h1>Projeto Vida&Carreira - Ingressão no Ensino Superior</h1></div>
+    return (
+        <div>
+            <Head>
+                <title>NomeProjeto</title>
+            </Head>
+            <nav>
+                <div>
+                    <h1>NomeProjeto</h1>
+                </div>
+                <div class='Menu'>
+                    <a>Inícior</a>
+                    <a>Ensino Superior</a>
+                    <a>Enem/Vestibular</a>
+                    <a>Referências</a>
+                    <a>Sugestões</a>
+                    <a>Contato</a>
+                </div>
+            </nav>
+            <div class='Corpo'>
+                <div class='imgHome'>
+                    
+                </div>
+                <div class='sobre'>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default Home;
